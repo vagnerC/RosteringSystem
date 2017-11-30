101a84822737a41cc6a182aa8af5285a483bfa89
Felipe Mello <felipe.silva.mello@gmail.com>
2017-11-30 06:08:51 +0000

    'test'

--- index.php
+++ index.php
@@ -6,6 +6,19 @@ require_once(TEMPLATE_PATH . "/header.php");
 require_once(TEMPLATE_PATH . "/menu_manager.php");
 ?>
 Here goes the code. you can use any HTML tags outside the php block or use them inside an echo "";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+</head>
+<body>
+	<div id="head"></div>
+	<div id="body"></div>
+	<div id="footer"></div>
+
+
+</body>
+</html>
 
 test sound
 <?php
