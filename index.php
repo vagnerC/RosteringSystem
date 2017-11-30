62682147ca031dd4f9509205ec5ea1f14333c915
bruno <bruno@bruno_san>
2017-11-30 09:53:53 +0000

    ,

--- index.php
+++ index.php
@@ -3,11 +3,25 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/RosteringSystem/resource/config.php'); 
 
 require_once(TEMPLATE_PATH . "/header.php");
-require_once(TEMPLATE_PATH . "/menu_manager.php");
-?>
-Here goes the code. you can use any HTML tags outside the php block or use them inside an echo "";
 
-test sound
+?>
+ <div class="container">
+
+      <form class="form-signin">
+        <label for="inputEmail" class="sr-only"></label><br />
+        <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus><br />
+        <label for="inputPassword" class="sr-only"></label><br />
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+      </form>
+
+    </div> <!-- /container -->
+
 <?php
 require_once(TEMPLATE_PATH . "/footer.php");
 ?>
