379c9f0abf5b8e2022d4aeb29bc8cbed27409010
Vagner Costa <vagnerc@ymail.com>
2018-02-01 12:34:48 +0000

    Calendar

--- calendar.php
+++ calendar.php
@@ -1,6 +1,6 @@
 <?php
 function draw_calendar($month,$year){
-    
+   
     /* draw table */
     $calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
     
--- roster_view.php
+++ roster_view.php
@@ -3,7 +3,7 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/RosteringSystem/resource/config.php');
 
 require_once(TEMPLATE_PATH . "/header.php");
-require_once("calendar.php");	
+require_once("calendar.php"); 	
   
     echo "<h2>Feb 2018</h2>";
 	echo draw_calendar(2,2018);
