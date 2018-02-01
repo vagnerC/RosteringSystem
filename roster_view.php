90570afca44465a37fa56553d3dcc56707e2d25d
Vagner Costa <vagnerc@ymail.com>
2018-02-01 12:33:24 +0000

    Calendar

--- roster_view.php
+++ roster_view.php
@@ -1,1 +1,12 @@
 <?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/RosteringSystem/resource/config.php');
+
+require_once(TEMPLATE_PATH . "/header.php");
+require_once("calendar.php");	
+  
+    echo "<h2>Feb 2018</h2>";
+	echo draw_calendar(2,2018);
+
+	require_once(TEMPLATE_PATH . "/footer.php");
+?>
