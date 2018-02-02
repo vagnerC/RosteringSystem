de86f8125545294fb35476fc28bd6cc52a38cf6a
Vagner Costa <vagnerc@ymail.com>
2018-02-02 11:06:29 +0000

    calendar updated

--- calendar.php
+++ calendar.php
@@ -5,6 +5,11 @@ function draw_calendar($month,$year){
     $calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
     
     /* table headings */
+    
+    $calendar .= '<tr class="calendar-row">';
+    $calendar .= '<td colspan="7" class="calendar-day-head">'.date('F')."/".date('Y').'</td>';
+    $calendar .= '</tr>';
+    
     $headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
     $calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
     
@@ -20,44 +25,42 @@ function draw_calendar($month,$year){
     
     /* print "blank" days until the first of the current week */
     for($x = 0; $x < $running_day; $x++):
-    $calendar.= '<td class="calendar-day-np"> </td>';
-    $days_in_this_week++;
+        $calendar.= '<td class="calendar-day-np"> </td>';
+        $days_in_this_week++;
     endfor;
     
     /* keep going with days.... */
     for($list_day = 1; $list_day <= $days_in_month; $list_day++):
-    $calendar.= '<td class="calendar-day">';
-    /* add in the day number */
-    $calendar.= '<div class="day-number">'.$list_day.'</div>';
+        $calendar.= '<td class="calendar-day">';
+        /* add in the day number */
+        $calendar.= '<div class="day-number">'.$list_day.'</div>';
     
-    /** QUERY THE DATABASE FOR AN ENTRY FOR THIS DAY !!  IF MATCHES FOUND, PRINT THEM !! **/
-    //$calendar.= str_repeat('<p></p>',2);
+        /** QUERY THE DATABASE FOR AN ENTRY FOR THIS DAY !!  IF MATCHES FOUND, PRINT THEM !! **/
+        if($year == 2018 and $month == 02 AND $list_day == 3):
+            $calendar.= '<p>John (09 - 18)</p>';
+        elseif($year == 2018 and $month == 02 AND $list_day == 20):
+            $calendar.= '<p>Mary (13 - 22)</p>';
+        else:
+            $calendar.= '<p></p>';
+        endif;
     
-    if($year == 2018 and $month == 02 AND $list_day == 3):
-        $calendar.= '<p>John (09 - 18)</p>';
-    elseif($year == 2018 and $month == 02 AND $list_day == 20):
-        $calendar.= '<p>Mary (13 - 22)</p>';
-    else:
-        $calendar.= '<p></p>';
-    endif;
-    
-    $calendar.= '</td>';
-    if($running_day == 6):
-    $calendar.= '</tr>';
-    if(($day_counter+1) != $days_in_month):
-    $calendar.= '<tr class="calendar-row">';
-    endif;
-    $running_day = -1;
-    $days_in_this_week = 0;
-    endif;
-    $days_in_this_week++; $running_day++; $day_counter++;
+        $calendar.= '</td>';
+        if($running_day == 6):
+            $calendar.= '</tr>';
+            if(($day_counter+1) != $days_in_month):
+                $calendar.= '<tr class="calendar-row">';
+            endif;
+            $running_day = -1;
+            $days_in_this_week = 0;
+        endif;
+        $days_in_this_week++; $running_day++; $day_counter++;
     endfor;
     
     /* finish the rest of the days in the week */
     if($days_in_this_week < 8):
-    for($x = 1; $x <= (8 - $days_in_this_week); $x++):
-    $calendar.= '<td class="calendar-day-np"> </td>';
-    endfor;
+        for($x = 1; $x <= (8 - $days_in_this_week); $x++):
+        $calendar.= '<td class="calendar-day-np"> </td>';
+        endfor;
     endif;
     
     /* final row */
--- roster_view.php
+++ roster_view.php
@@ -5,8 +5,7 @@ require_once(__ROOT__.'/RosteringSystem/resource/config.php');
 require_once(TEMPLATE_PATH . "/header.php");
 require_once("calendar.php"); 	
   
-    echo "<h2>Feb 2018</h2>";
-	echo draw_calendar(2,2018);
+	echo draw_calendar(date('m'), date('Y'));
 
 	require_once(TEMPLATE_PATH . "/footer.php");
 ?>
