8b878abc6f27b13ac27a1ecb532eb3d41bf8a41f
Luciene <nenirodrigues@hotmail.com>
2018-02-09 10:30:38 +0000

    roster

---
+++ roster_view.php
@@ -0,0 +1,13 @@
+<?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/RosteringSystem/resource/config.php');
+
+require_once(TEMPLATE_PATH . "/header.php");
+require_once("calendar.php");
+
+	echo draw_calendar(date('m'), date('Y'));
+	echo "<p>";
+	echo draw_calendar(date('m')+4, date('Y'));
+
+	require_once(TEMPLATE_PATH . "/footer.php");
+?>
