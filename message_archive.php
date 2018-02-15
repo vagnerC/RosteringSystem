9c3002243009d02fefd58e8cfb4619f2ab45db95
tbrizolla <32957789+tbrizolla@users.noreply.github.com>
2018-02-15 21:10:36 +0000

    Add files via upload

--- message_archive.php
+++ message_archive.php
@@ -1,1 +1,91 @@
 <?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/RosteringSystem/resource/config.php');
+
+require_once(TEMPLATE_PATH . "/header.php");
+?>
+	
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+
+<!--Font Awesome (added because you use icons in your prepend/append)-->
+<link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
+
+<!-- Custom CSS -->
+<style>
+.othertop{margin-top:10px;}
+
+.btn-search {
+	  background: #424242;
+	  border-radius: 0;
+	  color: #fff;
+	  border-width: 1px;
+	  border-style: solid;
+	  border-color: #1c1c1c;
+	}
+.btn-search:link, .btn-search:visited {
+	  color: #fff;
+	}
+.btn-search:active, .btn-search:hover {
+	  background: #1c1c1c;
+	  color: #fff;
+	}
+</style>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 ">
+			<form class="form-horizontal">
+			<fieldset>
+
+<!-- Form Name -->
+<legend>Message Archive</legend><br>
+
+<div class="container">
+<div class="input-group">
+	<input type="text" class="form-control" placeholder="Search for...">
+      <span class="input-group-btn">
+        <button class="btn btn-search" type="button"><i class="fa fa-search fa-fw"></i> Search</button>
+      </span>
+</div><br><br>
+<!-- Table -->
+<div>
+<div>
+<table class="table table-hover">
+  <thead>
+    <tr class="table-secondary">
+      <th scope="col">Subject</th>
+      <th scope="col">To</th>
+      <th scope="col">Date</th>		
+    </tr>
+  </thead>
+    <tr>
+	<td>
+	<input type="checkbox" name="type">
+	</td>	
+    </tr>
+
+	
+</div>
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
+
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+
+
+<?php 
+require_once(TEMPLATE_PATH . "/footer.php");
+?>
