051008a5e90d01a6feead993f6cf53ecd7664fc8
tbrizolla <32957789+tbrizolla@users.noreply.github.com>
2018-02-15 19:20:12 +0000

    Add files via upload

--- message_new.php
+++ message_new.php
@@ -1,1 +1,110 @@
 <?php
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/RosteringSystem/resource/config.php');
+
+require_once(TEMPLATE_PATH . "/header.php");
+?>
+	
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+
+<!--Font Awesome (added because you use icons in your prepend/append)-->
+<link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
+
+<!-- Custom CSS -->
+<style>
+.othertop{margin-top:10px;}
+
+</style>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 ">
+			<form class="form-horizontal">
+			<fieldset>
+
+<!-- Form Name -->
+<legend>Message</legend>
+
+<!-- Text input-->
+<div class="form-group">
+	<label class="col-md-4 control-label" for=“sendTo”>Send to:</label>  
+    <div class="col-md-4">
+		<div class="input-group">
+			<div class="input-group-addon">
+     		<i class="fa fa-envelope-o"></i>
+			</div>
+    		<input id="emailAddress" name=“Send To“ type="text" placeholder="Email Address" class="form-control input-md" value="<?php echo $emailAddress;?>">
+		</div>
+ 	</div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+	<label class="col-md-4 control-label" for=“subject”>Subject</label>  
+    <div class="col-md-4">
+ 		<div class="input-group">
+			<div class="input-group-addon">
+			<i class="fa fa-male" style="font-size: 20px;"></i>
+       	    </div>
+       		<input id=“subject” name=“subject” type="text" placeholder=“Subject” class="form-control input-md" value="<?php echo $nextOfKinName;?>">
+        </div>
+    </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+	<label class="col-md-4 control-label" for="Overview (max 200 words)">Message (max 200 words)</label>
+    <div class="col-md-4">                     
+    	<textarea class="form-control" rows="10"  id="Overview (max 200 words)" name="Overview (max 200 words)">Overview</textarea>
+  	</div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" ></label>  
+  <div class="col-md-4">
+  <input class="btn btn-primary" type=“submit” value="Send">
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div>
+
+
+	</div>
+	</div>
+
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
+
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+<script>
+	$(document).ready(function(){
+		var date_input=$('input[name="date"]'); //our date input has the name "date"
+		var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+		date_input.datepicker({
+			format: 'dd/mm/yyyy',
+			container: container,
+			todayHighlight: true,
+			autoclose: true,
+		})
+	})
+</script>
+
+<?php 
+require_once(TEMPLATE_PATH . "/footer.php");
+?>
