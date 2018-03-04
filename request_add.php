cf8c52c0fa1c4902c7e22fe8363541baeec52b18
Bruno Santos <brunorosan@gmail.com>
2018-03-04 19:19:57 +0000

    ;

--- request_add.php
+++ request_add.php
@@ -26,11 +26,12 @@ require_once(__ROOT__.'/RosteringSystem/resource/config.php');
     <style>
        .othertop{margin-top:10px;}
     </style>
+    
 </head>
 
 <body>
 
-   <div class="container">
+  <div class="container">
 <div class="row">
 <div class="col-md-10 ">
 <form class="form-horizontal">
@@ -41,7 +42,110 @@ require_once(__ROOT__.'/RosteringSystem/resource/config.php');
 
 <!-- Text input-->
 
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
 
+    <title>Better One Month Calendar - Bootsnipp.com</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+    <style type="text/css">
+    
+    </style>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        window.alert = function(){};
+        var defaultCSS = document.getElementById('bootstrap-css');
+        function changeCSS(css){
+            if(css) $('head > link').filter(':first').replaceWith('<link rel="stylesheet" href="'+ css +'" type="text/css" />'); 
+            else $('head > link').filter(':first').replaceWith(defaultCSS); 
+        }
+        $( document ).ready(function() {
+          var iframe_height = parseInt($('html').height()); 
+          window.parent.postMessage( iframe_height, 'https://bootsnipp.com');
+        });
+    </script>
+</head>
+<body>
+	<div class="container">
+	<div class="row">
+		<div class="span12">
+    	    <table class="table-condensed table-bordered table-striped">
+                <thead>
+                    <tr>
+                      <th colspan="7" >
+                        <a class="btn"><i class="icon-chevron-left"></i></a>
+                        <a class="btn">February 2012</a>
+                        <a class="btn"><i class="icon-chevron-right"></i></a>
+                      </th>
+                    </tr>
+                    <tr>
+                        <th>Su</th>
+                        <th>Mo</th>
+                        <th>Tu</th>
+                        <th>We</th>
+                        <th>Th</th>
+                        <th>Fr</th>
+                        <th>Sa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="muted">29</td>
+                        <td class="muted">30</td>
+                        <td class="muted">31</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                    </tr>
+                    <tr>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>14</td>
+                        <td>15</td>
+                        <td>16</td>
+                        <td>17</td>
+                        <td>18</td>
+                    </tr>
+                    <tr>
+                        <td>19</td>
+                        <td><strong>20</strong></td>
+                        <td>21</td>
+                        <td>22</td>
+                        <td>23</td>
+                        <td>24</td>
+                        <td>25</td>
+                    </tr>
+                    <tr>
+                        <td>26</td>
+                        <td>27</td>
+                        <td>28</td>
+                        <td>29</td>
+                        <td class="muted">1</td>
+                        <td class="muted">2</td>
+                        <td class="muted">3</td>
+                    </tr>
+                </tbody>
+            </table>    
+		</div>
+	</div>
+</div>
+	<script type="text/javascript">
+	
+	</script>
+</body>
+</html>
 <div class="form-group">
   <label class="col-md-4 control-label" for="type">Type</label>
   <div class="col-md-4">
