395523fd362d30d7726644b90c4b2607b7e97855
Vagner Costa <vagnerc@ymail.com>
2018-03-15 14:35:13 +0000

    .

--- roster_view.php
+++ roster_view.php
@@ -8,7 +8,8 @@ require_once("calendar.php");
 	<div id="size" class="calendarBox">
 	<?php 	echo draw_calendar(date('m'), date('Y')); ?>
 	</div>
-
 <?php
 	require_once(TEMPLATE_PATH . "/footer.php");
 ?>
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
