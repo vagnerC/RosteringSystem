c8a1c13be17fbadca7f41ba4451aaf6ec6ccb546
Vagner Costa <vagnerc@ymail.com>
2018-03-15 10:06:16 +0000

    .

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ session_start();
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__ . "/RosteringSystem/resource/config.php");
 require_once(TEMPLATE_PATH . "/header.php");
+if(isset($_SESSION['user_info'])):
+    header("Location: home.php");
+    die();
+endif;
 ?>
 <!-- Bootstrap Core CSS -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
--- payment_view.php
+++ payment_view.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
 define('__ROOT__', dirname(dirname(__FILE__)));
-require_once(__ROOT__.'/RosteringSystem/resource/config.php');
+require_once(__ROOT__ . "/RosteringSystem/resource/config.php");
 require_once(TEMPLATE_PATH . "/header.php");
-require_once("calendar_payment.php"); 	
+require_once("calendar_payment.php");
+if(!isset($_SESSION['user_info'])):
+    header("Location: index.php");
+    die();
+endif;
     
 	echo draw_calendar(date('m'), date('Y'));
 
