4925957dd0af7e99e56b700f5539ae581672b93f
Luciene <nenirodrigues@hotmail.com>
2018-04-22 22:42:55 +0100

    profile php

--- staff_profile.php
+++ staff_profile.php
@@ -1,20 +1,34 @@
 <?php
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/RosteringSystem/resource/config.php');
-
 require_once(TEMPLATE_PATH . "/header.php");
 
-$name = $surname = $email = $telephone = $nextOfKinName = $nextOfKinPhone = $minHour = $maxHour = $department = $position = $availability ="";
+if(!isset($_SESSION['user_info'])):
+echo "<script>location.href = 'index.php';</script>";
+die();
+elseif($_SESSION['user_info']['management'] != "true"):
+echo "<script>location.href = 'logout.php';</script>";
+die();
+endif;
+
+$idStaff            = "";
+
+$sql                = " SELECT *,
+						DATE_FORMAT(dateOfBirth, '%d/%m/%Y') AS dateOfBirth
+						FROM staff
+						WHERE idStaff = '$idStaff'";
+
+$sth = $DBH->prepare($sql);
+$sth->execute();
+$row = $sth->fetch(PDO::FETCH_OBJ);
 
 ?>
 <!-- Bootstrap Core CSS -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
-
 <!--Font Awesome (added because you use icons in your prepend/append)-->
 <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
-
 <!-- Custom CSS -->
 <style>
 .othertop{margin-top:10px;}
@@ -25,137 +39,108 @@ $name = $surname = $email = $telephone = $nextOfKinName = $nextOfKinPhone = $min
 	<div class="row">
 		<div class="col-md-10 ">
 			<form class="form-horizontal">
-			<fieldset>
+				<fieldset>
 
-<!-- Form Name -->
-<legend>User profile </legend>
+			<!-- Form Name -->
+					<legend>User profile </legend>
 
-<!-- Text input-->
-<div class="form-group">
-  	<label class="col-md-4 control-label" for="firstName">First Name: </label>  
-  	<div class="col-md-4">
- 		<div class="input-group">
- 		
-       	</div>
-	</div>
-</div>
+					<div class="form-group">
+						<label class="col-md-4 control-label"></label>
+						<div class="col-md-4">
+							<div id="info"></div> 
+						</div>
+					</div>
+					
+					<div class="form-group">
+  						<label class="col-md-4 control-label" for="name">* First Name </label>  
+  						<div class="col-md-4">
+ 							<div class="input-group">
+       							<div class="input-group-addon">
+        								<i class="fa fa-user"></i>
+       							</div>
+       							<input disabled id="name" name="name" type="text" placeholder="First Name" class="form-control input-md" value="<?php echo $row->name; ?>">
+     	 					</div>
+						</div>
+					</div>
 
-<!-- Text input-->
-<div class="form-group">
- 	<label class="col-md-4 control-label" for="lastName">Last Name: </label>  
-  	<div class="col-md-4">
- 		<div class="input-group">
-        	
-		</div>
-	</div>
-</div>
+                    <div class="form-group">
+						<label class="col-md-4 control-label" for="surname">* Last Name</label>  
+                      	<div class="col-md-4">
+                     		<div class="input-group">
+                            		<div class="input-group-addon">
+                            			<i class="fa fa-user"></i>
+                            		</div>
+                           		<input disabled id="surname" name="surname" type="text" placeholder="Last Name" class="form-control input-md" value="<?php echo $row->surname; ?>">
+                    			</div>
+                    		</div>
+                    </div>
 
-<!-- Text input-->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="dateOfBirth">Date Of Birth: </label>  
-  	<div class="col-md-4">
-		<div class="input-group">
+                    <div class="form-group">
+                    		<label class="col-md-4 control-label" for="dateOfBirth">* Date Of Birth</label>  
+                      	<div class="col-md-4">
+                    			<div class="input-group">
+                            		<div class="input-group-addon">
+                             		<i class="fa fa-calendar"></i>
+                            		</div>
+                            		<input disabled class="form-control" id="dateOfBirth" name="dateOfBirth" placeholder="MM/DD/YYYY" type="text"/  value="<?php echo $row->dateOfBirth; ?>">
+                          	</div>
+						</div> 
+                    </div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="phoneNumber">Phone number </label>  
+  						<div class="col-md-4">
+  							<div class="input-group">
+       							<div class="input-group-addon">
+    	    								<i class="fa fa-phone"></i>
+            						</div>
+    								<input id="phoneNumber" name="phoneNumber" type="text" placeholder="Phone number " class="form-control input-md" value="<?php echo $row->phoneNumber; ?>">
+        						</div>
+    						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="email">* Email Address</label>  
+    						<div class="col-md-4">
+							<div class="input-group">
+								<div class="input-group-addon">
+     								<i class="fa fa-envelope-o"></i>
+								</div>
+    								<input id="email" name="email" type="email" placeholder="Email Address" class="form-control input-md" value="<?php echo $row->email; ?>">
+							</div>
+ 						</div>
+					</div>
+
+                    <div class="form-group">
+                    		<label class="col-md-4 control-label" for="nextOfKinName">Next of kin Name</label>  
+						<div class="col-md-4">
+                     		<div class="input-group">
+                    				<div class="input-group-addon">
+                    					<i class="fa fa-male" style="font-size: 20px;"></i>
+                           	    </div>
+                           		<input id="nextOfKinName" name="nextOfKinName" type="text" placeholder="Next of Kin Name" class="form-control input-md" value="<?php echo $row->nextOfKinName; ?>">
+							</div>
+						</div>
+                    </div>
+
+                    <div class="form-group">
+                     	<label class="col-md-4 control-label" for="nextOfKinPhoneNumber"> Next of Kin Phone Number </label>  
+                      	<div class="col-md-4">
+                      		<div class="input-group">
+                           		<div class="input-group-addon">
+                         			<i class="fa fa-phone"></i>
+								</div>
+                        			<input id="nextOfKinPhoneNumber" name="nextOfKinPhoneNumber" type="text" placeholder="Next of Kin Phone number " class="form-control input-md"  value="<?php echo $row->nextOfKinPhoneNumber; ?>">
+                     		</div>
+						</div>
+					</div>
+	
 			
-      	</div> 
-      	
-	</div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="phoneNumber ">Phone number: </label>  
-  	<div class="col-md-4">
-  		<div class="input-group">
-        </div>
-			<div class="input-group othertop">
-	        </div>
+				</fieldset>
+			</form>
 		</div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="emailAddress">Email Address: </label>  
-    <div class="col-md-4">
-		<div class="input-group">
-		</div>
- 	</div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="nextOfKinName">Next of kin Name: </label>  
-    <div class="col-md-4">
- 		<div class="input-group">
-		
-		</div>
-    </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
- 	<label class="col-md-4 control-label" for="nextOfKinPhoneNumber"> Next of Kin Phone Number:  </label>  
-  	<div class="col-md-4">
-  		<div class="input-group">
-       		
- 		</div>
 	</div>
 </div>
-
-<!-- Multiple Radios (inline) -->
-<div class="form-group">
- 	<label class="col-md-4 control-label" for="department">Department: </label>
-  	<div class="col-md-4"> 
-    	
-	</div>
-</div>
-
-<!-- Multiple Radios (inline) -->
-<div class="form-group">
- 	<label class="col-md-4 control-label" for="radios">Position: </label>
-  	<div class="col-md-4"> 
-	    
-	</div>
-</div>
-
-<!-- Input number-->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="address)">Minimum Hour Available: </label>
-  	<div class="col-md-4">                     
- 	</div>
-</div>
-
-<!-- Input number -->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="address)">Maximum Hour Available:</label>
-  	<div class="col-md-4">                     
- 	</div>
-</div>
-<!-- Multiple Checkboxes -->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="availability">Days Available: </label>
-  	<div class="col-md-4">
-  	
- 	</div>	    
-</div>
-
-<!-- Textarea -->
-<div class="form-group">
-	<label class="col-md-4 control-label" for="Overview (max 200 words)">Notes: </label>
-    <div class="col-md-4">                     
-
-  	</div>
-</div>
-
-</fieldset>
-</form>
-</div>
-
-<div class="col-md-2 hidden-xs">
-	<img src="http://websamplenow.com/30/userprofile/images/avatar.jpg" class="img-responsive img-thumbnail ">
-</div>
-
-	</div>
-	</div>
 
 
     <!-- jQuery Version 1.11.1 -->
