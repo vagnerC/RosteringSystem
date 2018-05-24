bafddf3fc1b95237fd6c015f48453feb88c9b9b6
Vagner Costa <vagnerc@ymail.com>
2018-05-24 15:29:31 +0100

    .

---
+++ roster_change_manager_action.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once("template/header.php");
+require_once("resource/database.php");
+
+if(!isset($_SESSION['user_info'])):
+    echo "<script>location.href = 'index.php';</script>";
+    die();
+elseif($_SESSION['user_info']['management'] != "true"):
+    echo "<script>location.href = 'logout.php';</script>";
+    die();
+endif;
+
+$idRoster       = $_GET['id'];
+$action         = $_GET['a'];
+$week           = $_GET['w'];
+
+
+if($idRoster != "" and $action != ""){
+    
+    //Delete
+    if($action == "d"):
+        try{
+            $sql = "DELETE FROM roster WHERE idRoster = ? LIMIT 1";
+            echo $sql."> ".$idRoster;
+//             $sth = $DBH->prepare($sql);
+//             $sth->bindParam(1, $idRoster, PDO::PARAM_INT);
+//             if($sth->execute()):
+//                 echo "<script>location.href = 'roster_change_manager.php?week=$week';</script>";
+//             else:
+//                 echo "ERROR!";
+//                 print_r($sth->errorInfo());
+//             endif;
+        } catch(PDOException $e) {echo $e;}
+    //Edit
+    elseif($action == "e"):
+    
+    endif;
+    
+}
+
+
+
+?>
