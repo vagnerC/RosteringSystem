1730b8778aa273afad80b730d2516d344d48a3c1
Vagner Costa <vagnerc@ymail.com>
2018-05-25 09:53:29 +0100

    .

--- template/header.php
+++ template/header.php
@@ -4,9 +4,9 @@
        <meta charset="utf-8" />
         <title>Rostering System</title>
               <link rel="icon" href="image/layout/favicon.ico">
-              <link rel="stylesheet" type="text/css" href="library/bootstrap.min.css">
-              <link rel="stylesheet" type="text/css" href="css/general.css">
               
+              <link rel="stylesheet" type="text/css" href="css/general.css">
+              <script type="text/javascript" src="library/jquery-3.3.1.min.js"></script>
               <script type="text/javascript" src="library/validation.min.js"></script>
               <script type="text/javascript" src="library/bootstrap.min.js"></script>
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
